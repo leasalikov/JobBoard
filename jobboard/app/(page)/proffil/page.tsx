b7c6eceeb5ac42c74d148ac56665adbf30b0ca6e
Lea Salikov <Hand4c18@mbjcomp.org.il>
2024-10-14 13:49:53 +0300

    home background

---
+++ jobboard/app/(page)/proffil/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "@/components/section/Nav";
+export default function Proffil() {
+
+    return (
+        <div>
+            <br/><br/><br/><br/><br/>
+            <p>My Proffil</p>
+        </div>
+    );
+}

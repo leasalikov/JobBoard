81a56980bbeb9c4a62e670908ebb7529869dde49
chayaflam <c0504113701@gmail.com>
2024-10-14 16:22:52 +0300

    prisma

---
+++ jobboard/app/api/Jobs/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server"
+import prisma from "../../../prisma/client"
+
+
+export async function Post(request: Request) {
+    try {
+        const body = await request.json();
+        const job = await prisma.jobs.create({
+            data: body
+        })
+        return NextResponse.json({ message: "success add job", success: true, job })
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to add job", success: false })
+    }
+}
+
+export async function DELETE(request: Request) {
+    try {
+        const body = await request.json();
+        const response = await prisma.jobs.delete({
+            data: body
+        })
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to delete job", success: false })
+
+    }
+}
+
+export async function PUT(request: Request) {
+    try {
+        const body = await request.json();
+        const response = await prisma.jobs.put({
+            data: body
+        })
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to update job", success: false })
+
+    }
+}
+
+export async function PUT(request: Request) {
+    try {
+        const body = await request.json();
+        const response = await prisma.jobs.put({
+            data: body
+        })
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to update job", success: false })
+
+    }
+}

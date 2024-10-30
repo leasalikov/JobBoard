fb7252b7aa631f76c79eb0d2905b38bd12f031a1
Kibelevich <Hand4c28@mbjcomp.org.il>
2024-10-30 10:47:37 +0200

    candidantes

--- jobboard/app/(page)/candidates/page.tsx
+++ jobboard/app/(page)/candidates/page.tsx
@@ -1,20 +1,19 @@
 import prisma from "../../../prisma/client";
 
 
-function Candidates({jobId}:any) {
-async function Candidates({id}:{id:string}) {
-    const candidates=await prisma.candidacies.findMany( {
-        where:{jobId:id},
-       }
+async function Candidates({ jobId }: { jobId: string }) {
+    const candidates = await prisma.candidacies.findMany({
+        where: { jobId: jobId },
+    }
     )
-  return (
+    return (
 
         <div className="mt-5">
             <h4 className="text-lg font-semibold">Candidates List</h4>
             {/* הצג את המועמדים כאן */}
             <p>jobid={jobId}</p>
         </div>
-    );}
+    );
 }
 
 export default Candidates

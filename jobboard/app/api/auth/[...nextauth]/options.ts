c05f18f024bf998199e1b6067f3958b3f3a20006
Kibelevich <Hand4c28@mbjcomp.org.il>
2024-11-04 18:57:18 +0200

    login

--- jobboard/app/api/auth/[...nextauth]/options.ts
+++ jobboard/app/api/auth/[...nextauth]/options.ts
@@ -39,33 +39,42 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: GOOGLE_CLIENT_ID!,
       clientSecret: GOOGLE_CLIENT_SECRET!,
-    }),
+    })
   ],
   callbacks: {
     async signIn({ user, account }) {
       if (!user || !user.email) return false;
       if (account?.provider === "google signIn") {
-        await prisma.users.update({
-          where: {
-            email: user.email,
-          },
-          data: {
-            name: user.name,
-            image: user.image,
-          }
-        })
+        try {
+          await prisma.users.update({
+            where: {
+              email: user.email,
+            },
+            data: {
+              name: user.name,
+              image: user.image,
+            }
+          })
+        } catch (error) {
+          throw new Error(error as string)
+        }
+
       }
       if (account?.provider === "google signUp") {
-        await prisma.users.create({
-          data: {
-            image: user.image,
-            email: user.email,
-            name: user.name,
-            phone: "",
-            status: "",
-            username: ""
-          }
-        })
+        try {
+          await prisma.users.create({
+            data: {
+              image: user.image,
+              email: user.email,
+              name: user.name,
+              phone: "",
+              status: "",
+              username: ""
+            }
+          })
+        } catch (error) {
+          throw new Error(error as string)
+        }
       }
       return true;
     },

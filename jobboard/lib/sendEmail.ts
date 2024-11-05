1648bedcaa53f6e98bd270f867f62c5ef8de55ee
chani weiner <chani03630@gmail.com>
2024-11-05 12:57:22 +0200

    one change

--- jobboard/lib/sendEmail.ts
+++ jobboard/lib/sendEmail.ts
@@ -60,13 +60,7 @@ export function sendPasswordChangeEmail(userEmail: string, otp: string) {
             `
     };
     transporterEmail(mailOptions);
-    // transporter.sendMail(mailOptions, function (error, info) {
-    //     if (error) {
-    //         console.log(error);
-    //     } else {
-    //         console.log('Email sent: ' + info.response);
-    //     }
-    // });
+
 }
 
 

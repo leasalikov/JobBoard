e98dc5aefacf4c860d52f3e5b6c1b0bea7748278
Lea Salikov <Hand4c18@mbjcomp.org.il>
2024-11-05 11:57:03 +0200

    side bar

--- jobboard/app/(page)/jobSearch/page.tsx
+++ jobboard/app/(page)/jobSearch/page.tsx
@@ -103,14 +103,14 @@ export default function JobSearch() {
                     <input
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         type="text"
-                        name="Keyword"
-                        placeholder="keyword"
+                        name="keyword"
+                        placeholder="Keyword"
                     />
                     <input
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         type="text"
-                        name="Location"
-                        placeholder="loction"
+                        name="location"
+                        placeholder="Loction"
                     />
                     <Select
                         selectionMode="multiple"

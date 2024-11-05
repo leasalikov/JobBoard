97507bbc7a59f5fd2359c3b4eaf18f9d88b49afd
ruthSalomon <ruth32608@gmail.com>
2024-11-05 16:27:40 +0200

    candidacies

--- jobboard/app/(page)/candidates/page.tsx
+++ jobboard/app/(page)/candidates/page.tsx
@@ -3,29 +3,24 @@ import prisma from "../../../prisma/client";
 
 
 async function Candidates({ jobId }: { jobId: string }) {
-    // const candidates= await prisma.candidacies.findMany({
-    //     where: { jobId: jobId },
-    //     include: {
-    //         user: true
-    //     }
-    // });
-         const response = await fetch(`http://localhost:3000/api/candidacies/${jobId}`);
-              const candidates = await response.json();
-              console.log(candidates);
-    
+
+    const response = await fetch(`http://localhost:3000/api/candidacies?jobId=${jobId}`);
+    const candidates = await response.json();
+    console.log(candidates);
+
     return (
         <div className="mt-5">
             <h4 className="text-lg font-semibold">Candidates List</h4>
-          
+
             {/* Display candidates here */}
-            {candidates.map((candidate:any) => (
+            {candidates.map((candidate: any) => (
                 <div className="w-1/3 px-3 mb-6">
                     <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">name: {candidate.user.username}</p>
                     <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">email: {candidate.user.email}</p>
                     <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">phone: {candidate.user.phone}</p>
                     <div className="relative mt-8 flex items-center gap-x-4">
-                    <img src={candidate.user.image as string} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
-                </div>
+                        <img src={candidate.user.image as string} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
+                    </div>
                 </div>
             ))}
         </div>

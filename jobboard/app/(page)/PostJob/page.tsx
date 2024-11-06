dfc2ec1d45313f8a988b0fec870e90cf94237797
chaya <chb8551@gmail.com>
2024-11-06 13:45:33 +0200

    post job

--- jobboard/app/(page)/PostJob/page.tsx
+++ jobboard/app/(page)/PostJob/page.tsx
@@ -91,60 +91,6 @@ export default function PostJobForm() {
                         )}
                     </div>
 
-                    {/* <div>
-                        <label htmlFor="geographicalLocation" className="block text-sm font-medium leading-6 text-gray-900">geographical Location</label>
-                        <div className="mt-2">
-                            <input id="geographicalLocation" name="geographicalLocation" type="gf" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="requiredExperienceLevel" className="block text-sm font-medium leading-6 text-gray-900">required Experience Level</label>
-                        <div className="mt-2">
-                            <input id="requiredExperienceLevel" name="requiredExperienceLevel" type="string" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="salaryOffered" className="block text-sm font-medium leading-6 text-gray-900">salary Offered</label>
-                        <div className="mt-2">
-                            <input id="salaryOffered" name="salaryOffered" type="string" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="jobType" className="block text-sm font-medium leading-6 text-gray-900">jobType</label>
-                        <div className="mt-2">
-                            <input id="jobType" name="jobType" type="string" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="field" className="block text-sm font-medium leading-6 text-gray-900">field</label>
-                        <div className="mt-2">
-                            <input id="field" name="field" type="string" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="jobTitle" className="block text-sm font-medium leading-6 text-gray-900">job Title</label>
-                        <div className="mt-2">
-                            <input id="jobTitle" name="jobTitle" type="string" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="requirements" className="block text-sm font-medium leading-6 text-gray-900">requirements</label>
-                        <div className="mt-2">
-                            <input id="requirements" name="requirements" type="string" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="jobDescription" className="block text-sm font-medium leading-6 text-gray-900">job Description</label>
-                        <div className="mt-2">
-                            <input id="jobDescription" name="jobDescription" type="tel" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="status" className="block text-sm font-medium leading-6 text-gray-900">status</label>
-                        <div className="mt-2">
-                            <input id="status" name="status" type="tel" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div> */}
                     <div className="col-span-full">
                         <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">Photo</label>
                         <div className="mt-2 flex items-center gap-x-3">
@@ -162,18 +108,7 @@ export default function PostJobForm() {
                             {/* <button type="button" className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button> */}
                         </div>
                     </div>
-                    {/* <div className="flex items-center justify-center w-full">
-                        <label className="flex flex-col items-center justify-center w-full h-50 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                </svg>
-                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload CV</span> or drag and drop</p>
-                                <p className="text-xs text-gray-500 dark:text-gray-400">PDF or DOC</p>
-                            </div>
-                            <input id="dropzone-file" type="file" className="hidden" />
-                        </label>
-                    </div> */}
+                  
 
                     <div className="sm:col-span-3">
                         <label htmlFor="experience" className="block text-sm font-medium leading-6 text-gray-900">Experience</label>
@@ -194,13 +129,13 @@ export default function PostJobForm() {
                 </form>
 
 
-                {/* 
+                
 
                 <p className="mt-10 text-center text-sm text-gray-500">
                     You can set and edit your profile information at any time<br />
                     <a href="/home" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Skip</a>
 
-                </p> */}
+                </p>
             </div>
         </div>
 

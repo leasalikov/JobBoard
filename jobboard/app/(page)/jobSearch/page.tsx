ac2733557bb705a230c70c93e0b304542590b6cf
chaya <chb8551@gmail.com>
2024-11-07 12:19:44 +0200

    modal

--- jobboard/app/(page)/jobSearch/page.tsx
+++ jobboard/app/(page)/jobSearch/page.tsx
@@ -197,25 +197,26 @@ export default function JobSearch() {
                                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Apply for a job</button>
                             </div>
+                            <div className="bg-white py-24 sm:py-32">
+                                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+
+                                    <button onClick={handleOpenModal} className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-modal-example" data-hs-overlay="#hs-modal-example">Applying</button>
+
+                                    {isModalOpen && <Modal onClose={handleCloseModal} />}
+                                </div>
+                            </div>
                         </div>
-                        
+
                     ))}
                 </div>
                 <div>
-            <div className="bg-white py-24 sm:py-32">
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
 
-                    <button onClick={handleOpenModal} className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-modal-example" data-hs-overlay="#hs-modal-example">Applying</button>
-
-                    {isModalOpen && <Modal onClose={handleCloseModal} />}
                 </div>
-            </div>
-        </div>
                 {apllyJob &&
-                <h1 className="text-slate-800 text-4xl font-semibold">
-                    aplly!!
-                </h1>
-            }
+                    <h1 className="text-slate-800 text-4xl font-semibold">
+                        aplly!!
+                    </h1>
+                }
             </div>
         </>
     )
